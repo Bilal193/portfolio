075de0bce180a1889c09b6930585a00fc7144ede
Bilal193 <muhammad.bilal@ecomexperts.io>
2024-02-01 17:08:21 +0500

    final commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 import data from "../assets/data.json";
 
@@ -19,17 +20,38 @@ const Work = () => {
                 interval={2000}
                 infiniteLoop={true}
                 autoPlay={false}
+                renderArrowPrev={(clickHandler, hasPrev) => {
+                    return (
+                      <div
+                        className={`${
+                          hasPrev ? 'absolute' : 'hidden'
+                        } buttondiv`}
+                        onClick={clickHandler}
+                      >
+                        <FaArrowCircleLeft className="previcon-style" />
+                      </div>
+                    );
+                  }}
+                  renderArrowNext={(clickHandler, hasNext) => {
+                    return (
+                      <div
+                        className={`${
+                          hasNext ? 'absolute' : 'hidden'
+                        } buttondivnext`}
+                        onClick={clickHandler}
+                      >
+                        <FaArrowCircleRight className="nexticon-style" />
+                      </div>
+                    );
+                  }}
                 >
                     {
                         data.projects.map(i=>(
                             <div key={i.title} className="workItem">
+                                 <h3>{i.title}</h3>
+                                 <a href={i.url} target='blank'>
                                 <img src={i.imgSrc} alt={i.title} />
-                                <aside>
-                                    <h3>{i.title}</h3>
-                                    <p>{i.description}</p>
-                                    <a href={i.url} target={'blank'}>Visit</a>
-                                </aside>
-
+                                </a>
                             </div>
                         ))
                     }
